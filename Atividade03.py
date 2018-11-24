23583e1e6ce8935da18c8b87546fd234dd0915cb
careduardosilva <car.eduardosilva@hotmail.com>
2018-11-24 01:09:47 -0300

    Update Atividade03.py

--- Atividade03.py
+++ Atividade03.py
@@ -19,12 +19,15 @@ class Estudante():
         
 def main():
     #Alunos
+    k = 0
     alunos = [Estudante("Ronaldo",313,0),Estudante("Ricardo",932,3),Estudante("Felipe",333,1),Estudante("Raissa",321,1)]
     n = input("Digite o nome de um aluno")
     for i in alunos:
         if(n == i.getNome()):
             i.addCreditos(1)
         else:
-            print("Aluno nao esta cadastrado")  
+            if(k >= len(alunos)):
+                print("Aluno nao esta cadastrado")  
+            k += 1
 if __name__ == "__main__":
     main()
