d6d7a1de73e5c6d1ade160ebd74d837eb783fea7
careduardosilva <car.eduardosilva@hotmail.com>
2018-12-09 02:45:01 -0300

    Update ClockDisplay.py

--- Classes/Interacao entre Objetos/ClockDisplay.py
+++ Classes/Interacao entre Objetos/ClockDisplay.py
@@ -6,19 +6,41 @@ class ClockDisplay:
     def __init__(self):
         self.__hours = NumberDisplay(24)
         self.__minutes = NumberDisplay(60)
+        self.__seconds = NumberDisplay(60)
         self.__updateDisplay()
 
     def __updateDisplay(self):
-        self.__displayString = self.__hours.getDisplayValue() + ":" + self.__minutes.getDisplayValue()
+        self.__displayString = self.__hours.getDisplayValue() + ":" + self.__minutes.getDisplayValue() + ":" + self.__seconds.getDisplayValue()
+    def timeTick(self):
+        self.__seconds.increment()
+        if(self.__seconds.getValue() == 0):
+            self.__minutes.increment()
+            if(self.__minutes.getValue() == 0):
+                self.__hours.increment()
+        self.__updateDisplay()
+    #Getters
     def getHours(self):
         return self.__hours.getDisplayValue()
     def getMinutes(self):
-        return self.__minutes
-    def getTime(self): #Atividade 3
+        return self.__minutes.getDisplayValue()
+    def getSeconds(self):
+        return self.__seconds.getDisplayValue()
+    def getTime(self):
         return self.__displayString
-    def setTime(self,hour,minute): #Atividade 3
+    #Setters
+    def setHours(self,replacementValue):
+        self.__hours.setValue(replacementValue)
+    def setMinutes(self,replacementValue):
+        self.__minutes.setValue(replacementValue)
+    def setSeconds(self,replacementValue):
+        self.__seconds.setValue(replacementValue)
+    def setTime(self,hour,minute,second):
         if(hour < 24 and hour >= 0):
            self.__hours.setValue(hour)
-        if (minute < 60 and hour >= 0):
+        if (minute < 60 and minute >= 0):
             self.__minutes.setValue(minute)
+        if(second < 60 and second >= 0):
+            self.__seconds.setValue(second)
         self.__updateDisplay()
+
+
