e7cc842d18a0a0fba5401de967f1812da7fc8cfb
careduardosilva <car.eduardosilva@hotmail.com>
2018-12-09 02:36:02 -0300

    Update principal2.py

--- Classes/Interacao entre Objetos/principal2.py
+++ Classes/Interacao entre Objetos/principal2.py
@@ -1,6 +1,7 @@
 from ClockDisplay import ClockDisplay
 
 def main():
+   #O relogio foi setado para 22:56 no construtor
    relogio = ClockDisplay()
    print(relogio.getTime())
    for i in range(124): #Acionei 124 vezes para chegar a 1 hora
