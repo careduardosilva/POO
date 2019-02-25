254741d2f0f9aafe15a3f5c96afb318d538755ed
careduardosilva <car.eduardosilva@hotmail.com>
2019-02-25 10:49:15 -0300

    Update Bicicleta.py

--- Classes/Polimorfismo/Bicicleta.py
+++ Classes/Polimorfismo/Bicicleta.py
@@ -1,4 +1,10 @@
+
 from Veiculo import *
 class Bicicleta(Veiculo):
-	def __init__(self,placaveiculo,fabricanteveiculo,numerorodas,tipoveiculo,marchas):
-		Veiculo.__init__(self, placaveiculo, fabricanteveiculo, numerorodas, tipoveiculo,marchas)
+	def __init__(self,placaveiculo,fabricanteveiculo,numerorodas,tipoveiculo, marcha):
+		Veiculo.__init__(self, placaveiculo, fabricanteveiculo, numerorodas, tipoveiculo)
+		self.__marcha = marcha
+	def getMarcha(self):
+		return self.__marcha
+	def setMarcha(self,novo):
+		self.__marcha = novo
