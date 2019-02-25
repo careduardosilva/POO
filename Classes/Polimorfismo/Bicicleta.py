ea18a26cc1e0506209fcb1d256a090d307825f60
careduardosilva <car.eduardosilva@hotmail.com>
2019-02-25 03:02:13 -0300

    Create Bicicleta.py

---
+++ Classes/Polimorfismo/Bicicleta.py
@@ -0,0 +1,4 @@
+from Veiculo import *
+class Bicicleta(Veiculo):
+	def __init__(self,placaveiculo,fabricanteveiculo,numerorodas,tipoveiculo,marchas):
+		Veiculo.__init__(self, placaveiculo, fabricanteveiculo, numerorodas, tipoveiculo,marchas)
